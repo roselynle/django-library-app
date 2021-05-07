94c7f25b84ba013fe95a9391a3840e82d0ef15d6
Roselyn Le <roselynle95@gmail.com>
2021-05-07 15:54:51 +0100

    add new path for creating books

--- borrowers/library/urls.py
+++ borrowers/library/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='library-home'),
     path('about/', views.about, name='library-about'),
     path('books/', views.books, name='library-books'),
+    path('books/new/', views.create, name='books-create'),
     path('books/<int:book_id>', views.show, name='library-show'),
 ]
