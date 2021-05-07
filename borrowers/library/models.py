fb265efbaec8ffc71217cffba50f824852f4b5d6
Elwin <elwin.carlos09@gmail.com>
2021-05-07 11:08:37 +0100

    add Author and Book models

--- borrowers/library/models.py
+++ borrowers/library/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.title} ({self.author.name})'
