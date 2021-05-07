9444582fffb3a2b3b6084bc6c397d7642bfa010d
Elwin <elwin.carlos09@gmail.com>
2021-05-07 08:54:58 +0100

    remove reference to library path from view returns

--- borrowers/library/views.py
+++ borrowers/library/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'library/home.html')
+    return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'library/about.html')
+    return render(request, 'about.html')
