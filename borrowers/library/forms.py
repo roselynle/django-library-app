d79b01c861c0f29d4270d703123decb23240c007
Roselyn Le <roselynle95@gmail.com>
2021-05-07 15:54:14 +0100

    add form for new book and borrow book

---
+++ borrowers/library/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Book
+
+class NewBookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']
+
+class BorrowBookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['borrower']
+        widgets = {'borrower': forms.HiddenInput()}
+
+class ReturnBookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['borrower']
+        widgets = {'borrower': forms.HiddenInput()}
